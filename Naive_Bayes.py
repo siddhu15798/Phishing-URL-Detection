d207096cd416a6b97ff246bb74e605a77165b195
Siddharth Aggarwal <31208939+siddhu15798@users.noreply.github.com>
2019-11-17 23:05:29 +0530

    Add files via upload

---
+++ Naive_Bayes.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from warnings import simplefilter
+from sklearn.model_selection import GridSearchCV
+from sklearn.naive_bayes import GaussianNB, BernoulliNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,accuracy_score
+
+simplefilter(action='ignore', category=FutureWarning)
+
+URLS = pd.read_csv("data.csv")
+
+# Phishing_URLS = pd.read_csv("Phishing-URLS.csv")
+
+# Legitimate_URLS = pd.read_csv("Legitimate-URLS.csv")
+
+# URLS = Legitimate_URLS.append(Phishing_URLS)
+
+URLS = URLS.drop(URLS.columns[[0]], axis=1)
+
+URLS = URLS.sample(frac=1).reset_index(drop=True)
+
+URLS_Without_Labels = URLS.drop('Result', axis=1)
+
+Labels = URLS['Result']
+
+Training_Data, Testing_Data = train_test_split(URLS_Without_Labels, test_size=0.25, random_state=110)
+
+Training_Labels, Testing_Labels = train_test_split(Labels, test_size=0.25, random_state=110)
+
+gb = GaussianNB()
+
+bn = BernoulliNB()
+
+Prediction_Labels = gb.fit(Training_Data, Training_Labels).predict(Testing_Data)
+
+Prediction_Labels_1 = bn.fit(Training_Data, Training_Labels).predict(Testing_Data)
+
+print("\nAccuracy Score obtained is : ",accuracy_score(Testing_Labels, Prediction_Labels))
+
+print("\nAccuracy Score obtained is : ",accuracy_score(Testing_Labels, Prediction_Labels_1))
